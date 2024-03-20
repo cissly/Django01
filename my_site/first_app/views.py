38ae032116eedbc1db7982a698f01b6437ed1fa0
cissly <tkdwls237@naver.com>
2024-03-20 12:28:16 +0900

    update template

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+# from django.http.response import HttpResponse,HttpResponseNotFound,Http404, HttpResponseRedirect, HttpResponseRedirectBase
+from django.urls import reverse
 # Create your views here.
 
-def simple_view(req) :
-    return HttpResponse("Simple view")
+def simple_view(req):
+    return render(req, 'first_app/example.html')
+
+
+
+# articles = {
+#     'sports': 'Sports Page',
+#     'finance': 'Finance Page',
+#     'politics': 'Politics Page'
+# }
+
+# def news_view(req, topic):
+#     try:
+#         result = articles[topic]
+#         return HttpResponse(articles[topic])
+#     except:
+#         # result = 'No page for that topic!'
+#         # return HttpResponseNotFound(result)
+#         raise Http404('404 GENERIC ERROR')
+
+# def add_view(req, num1, num2):
+#     result = num1 +num2
+#     return HttpResponse(str(result))
+
+
+# def num_page_view(req, num_page):
+#     topics_list = list(articles.keys())
+#     topic = topics_list[num_page]
+
+#     #webpage = reverse('topic-page',args=[topic])
+#     return HttpResponseRedirect(reverse('topic-page',args=[topic]))
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!gk&8jw!vnrl#+_o!mlr0#y$qunn10hg42py8kovf&_9e1dqc)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = "my_site.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR,'templates/')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
